0dbdbd1f1f68cfe5fc02db5e6b7abbeb9c7cd66d
RaymondHe <2224928976@qq.com>
2025-05-18 19:35:22 -0500

    <Fix>Fix

--- paypay_backend/settings.py
+++ paypay_backend/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -85,8 +89,12 @@ WSGI_APPLICATION = 'paypay_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Paypay',  # 数据库名
+        'USER': 'admin',  # 用户名
+        'PASSWORD': 'Paypay123',
+        'HOST': 'mysql-paypay.czekgscqsz4j.us-east-2.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
